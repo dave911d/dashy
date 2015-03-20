9550c26e70d92f73fc27e9496c08ef56a1811816
root <davidpatch98@gmail.com>
2015-03-20 21:30:42 +1100

    moved to jade successfully(?)
    Conflicts:
    	views/home.handlebars
    	views/layouts/main.handlebars

--- dashboard.js
+++ dashboard.js
@@ -1,15 +1,14 @@
 'use strict';
 /*eslint-env node, mocha */
-/*eslint quotes: [2, "single"], curly: 2, camelcase: 1*/
+/*eslint quotes: [2, "single"], curly: 2, camelcase: 1, no-warning-comments:1*/
 var express = require('express');
 var app = express();
 require('./routes/routes.js')(app);
 require('./errorhandling/uncaughtexception.js')(app);
-var handlebars = require('express-handlebars')
-    .create({defaultLayout: 'main'});
-app.engine('handlebars', handlebars.engine);
-    app.set('view engine', 'handlebars');
-    app.set('port', process.env.PORT || 3000);
+app.engine('jade', require('jade').__express);
+    app.set('view engine', 'jade');
+    app.set('views', './views');
+    app.set('port', process.env.PORT || 3001);
 
 
 app.listen(app.get('port'), function(){
--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
 
   //the main page
   app.get('/', function(req, res){
-    res.render('home');
+    res.render('index', {title: 'Title', message: 'Dashboard'});
   });
 
 //the 404 page
