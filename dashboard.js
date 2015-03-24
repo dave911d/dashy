bab17b65a2d2b78ea572d60da8866637aeb8c2c2
david johns <davidpatch98@gmail.com>
2015-03-24 13:48:15 +1100

    add some comments on the dashboard and implement CSS for index

--- dashboard.js
+++ dashboard.js
@@ -1,18 +1,17 @@
 'use strict';
 /*eslint-env node, mocha */
 /*eslint quotes: [2, "single"], curly: 2, camelcase: 1, no-warning-comments:1 no-underscore-dangle:0*/
-
+//These are for my eslint setup
 var express = require('express');
 var app = express();
-require('./routes/routes.js')(app);
-require('./errorhandling/uncaughtexception.js')(app);
+require('./routes/routes.js')(app); //sets locations for routes
+require('./errorhandling/uncaughtexception.js')(app); //run if uncaughtexception
 app.engine('jade', require('jade').__express);
     app.set('view engine', 'jade');
-    app.set('views', './views');
-    app.set('port', process.env.PORT || 3002);
+    app.set('port', process.env.PORT || 3002); //Designates port to run on
 
 
 app.listen(app.get('port'), function(){
     console.log('Express is running on http://localhost:' + app.get('port') + '; press Ctrl-c to end');
     console.log('at least its working dude');
-  });
+  });//starts the server on specified port
