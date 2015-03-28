a18a5619e95112a2d1d8966e6e23560efeb2c37f
david johns <davidpatch98@gmail.com>
2015-03-28 17:48:58 +1100

    dd

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ To run simply:
 1.cd to the directory
 2.run npm install (possibly as sudo or administrator if problems arise)
 3.launch your redis server
-4.define the port and IP of said redis server in your redisconfig/configRedis.js file.
+4.define the port and IP of said redis server in your redisconfig/configRethinkDb.js file.
 5.run node dashboard.js
--- dashboard.js
+++ dashboard.js
@@ -2,24 +2,15 @@
 /*eslint-env node, mocha */
 /*eslint quotes: [2, "single"], curly: 2, camelcase: 1, no-warning-comments:1 no-underscore-dangle:0*/
 //These are for my eslint setup
+
+
 var express = require('express'); //requires express,our HTTP lib
 var app = express(); //initiates express
+require('./routes/routes.js')(app);
+//var login = require('./redisconfig/configRethinkDb.js');
+require('./errorHandling/uncaughtException.js')(app);//run if uncaughtException
 
-var redis = require('redis'); //requires redis, need this for data monitoring
-var adapter = require('socket.io-redis');
-var socket = require('socket.io')(3000);
-var http = require('http');
-var login = require('./redisconfig/configRedis.js');
-//this file contains port and ip of redis server
 
-var client = redis.createClient(login.portRedis(), login.ipRedis(), {});
-//login is defined in configRedis.js
- socket.adapter(adapter({host: login.ipRedis(), port: login.portRedis()}));
- var pub = redis.createClient(login.portRedis(), login.ipRedis(), {});
- var sub = redis.createClient(login.portRedis(), login.ipRedis(), {});
- socket.adapter(adapter({pubClient: pub, subClient: sub}));
-
-//run if uncaughtexception
 app.engine('jade', require('jade').__express);
   app.set('view engine', 'jade');
   //sets jade,need this to display to browser
@@ -27,13 +18,8 @@ app.engine('jade', require('jade').__express);
    //Designates port to run on
 
 
-   require('./socketIo/socketIo.js')(socket);//check this
-   require('./redisconfig/redisTest.js')(client);
-   //require('./redisconfig/redisConfigSub.js')(sub);
-   require('./redisconfig/redisMain.js')(pub);
-   require('./routes/routes.js')(app);
-   //sets locations for routes
-   require('./errorhandling/uncaughtexception.js')(app);
+
+//sets locations for routes
 app.listen(app.get('port'), function(err){
     if (err) {
       console.log(err.stack);
